3d175929b8412630f7986c340b22e1875adcc2a0
Justin Baylis <jlukebay@gmail.com>
2023-10-02 13:26:53 -0400

    Initial Commit

---
+++ newInventoryApp/newInventoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  newInventoryAppApp.swift
+//  newInventoryApp
+//
+//  Created by  on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct newInventoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
